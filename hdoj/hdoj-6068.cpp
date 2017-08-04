76500201b804f849f9444e9729957aaaf55f4bd4
fshp971 <fshp971@gmail.com>
2017-08-04 15:46:39 +0800

    Update hdoj-6068.cpp

--- hdoj/hdoj-6068.cpp
+++ hdoj/hdoj-6068.cpp
@@ -12,7 +12,7 @@ typedef pair<int,int> pii;
 const int maxn = 100000 + 5;
 const int maxm = 200 + 5;
 
-void KMP(char *s, int n, char *t, int m, LL fg[][maxm]) {
+void KMP(char *s, int n, char *t, int m, LL ff[][maxm]) {
 	static int u, fail[maxn + 2];
 	fail[1] = 0;
 	for(int i = 2; i <= m; ++i) {
@@ -24,19 +24,21 @@ void KMP(char *s, int n, char *t, int m, LL fg[][maxm]) {
 	u = 0;
 	for(int i = 1; i <= n; ++i) {
 		for(int k = 0; k <= m; ++k)
-			fg[i][k] = 0;
+			ff[i][k] = ff[i - 1][k];
 		while(u && s[i - 1] != t[u])
 			u = fail[u];
 		if(s[i - 1] == t[u])
 			++u;
 		int v = u;
 		while(v) {
-			fg[i][v] = 1;
+			++ff[i][v];
 			v = fail[v];
 		}
 		if(u == m)
 			u = fail[u];
 	}
+	for(int i = 1; i <= n; ++i)
+		ff[i][m] += ff[i - 1][m];
 }
 
 LL f[2][maxn][maxm];
@@ -51,25 +53,9 @@ int main() {
 		scanf("%s", tt);
 
 		KMP(ss, n, tt, m, f[0]);
-		for(int k = 0; k <= m; ++k) {
-			f[0][0][k] = 0;
-			for(int i = 1; i <= n; ++i)
-				f[0][i][k] += f[0][i - 1][k];
-		}
-		for(int i = 1; i <= n; ++i)
-			f[0][i][m] += f[0][i - 1][m];
-
 		reverse(ss, ss + n);
 		reverse(tt, tt + m);
-
 		KMP(ss, n, tt, m, f[1]);
-		for(int k = 0; k <= m; ++k) {
-			f[1][0][k] = 0;
-			for(int i = 1; i <= n; ++i)
-				f[1][i][k] += f[1][i - 1][k];
-		}
-		for(int i = 1; i <= n; ++i)
-			f[1][i][m] += f[1][i - 1][m];
 
 		while(kk--) {
 			scanf("%d%d", &l, &r);
