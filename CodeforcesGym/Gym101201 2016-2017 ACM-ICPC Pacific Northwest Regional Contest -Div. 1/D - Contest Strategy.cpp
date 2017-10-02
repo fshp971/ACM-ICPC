6ab789aace0984d43999e713ab399eeccd7a0271
fshp971 <fshp971@gmail.com>
2017-10-02 09:53:55 -0500

    Update D - Contest Strategy.cpp

--- CodeforcesGym/Gym101201 2016-2017 ACM-ICPC Pacific Northwest Regional Contest -Div. 1/D - Contest Strategy.cpp
+++ CodeforcesGym/Gym101201 2016-2017 ACM-ICPC Pacific Northwest Regional Contest -Div. 1/D - Contest Strategy.cpp
@@ -18,30 +18,30 @@ LL Fac[maxn + 5], f[2][maxn + 5];
 bool cmp(const int &a, const int &b) { return a > b; }
 
 int main() {
-	Fac[0] = 1;
-	for(int i = 1; i <= maxn; ++i) Fac[i] = Fac[i-1] * i % md;
-	scanf("%d%d", &n, &kk);
-	for(int i = 1; i <= n; ++i) scanf("%d", aa + i);
-	sort(aa + 1, aa + 1 + n, cmp);
-	int par = 0;
+    Fac[0] = 1;
+    for(int i = 1; i <= maxn; ++i) Fac[i] = Fac[i-1] * i % md;
+    scanf("%d%d", &n, &kk);
+    for(int i = 1; i <= n; ++i) scanf("%d", aa + i);
+    sort(aa + 1, aa + 1 + n, cmp);
+    int par = 0;
     LL ssum = 0;
-	mem(f[par], 0);
-	for(int i = 1; i <= kk; ++i) {
+    mem(f[par], 0);
+    for(int i = 1; i <= kk; ++i) {
         ssum = (ssum + aa[kk - i + 1]) % md;
-		f[par][i + kk - 1] = Fac[kk] * ssum % md;
+        f[par][i + kk - 1] = Fac[kk] * ssum % md;
     }
-	for(int i = kk + 1; i <= n; ++i) {
-		par ^= 1, mem(f[par], 0);
-		for(int j = kk; j <= i - 1; ++j) {
-			f[par][j] = (Fac[i-1] * aa[i] % md + f[par^1][j-1]) % md * j % md;
-			f[par][j] = (f[par][j] + f[par^1][j] * (i - j) % md) % md;
-		}
-		for(int j = i; j <= kk + i - 1; ++j)
-			f[par][j] = (Fac[i-1] * aa[i] % md + f[par^1][j-1]) % md * i % md;
-	}
-	LL ans = 0;
-	for(int i = kk; i <= kk + n - 1; ++i)
-		ans = (ans + f[par][i]) % md;
-	printf("%lld\n", ans);
-	return 0;
+    for(int i = kk + 1; i <= n; ++i) {
+        par ^= 1, mem(f[par], 0);
+        for(int j = kk; j <= i - 1; ++j) {
+            f[par][j] = (Fac[i-1] * aa[i] % md + f[par^1][j-1]) % md * j % md;
+            f[par][j] = (f[par][j] + f[par^1][j] * (i - j) % md) % md;
+        }
+        for(int j = i; j <= kk + i - 1; ++j)
+            f[par][j] = (Fac[i-1] * aa[i] % md + f[par^1][j-1]) % md * i % md;
+    }
+    LL ans = 0;
+    for(int i = kk; i <= kk + n - 1; ++i)
+        ans = (ans + f[par][i]) % md;
+    printf("%lld\n", ans);
+    return 0;
 }
